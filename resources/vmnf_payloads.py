24b289ac2a4d3242a9cd5f9c19e8018e635e7a05
s4dhul4bs <s4dhu@tutanota.com>
2020-12-12 10:31:03 -0300

    adds module for generating fuzzer payloads

---
+++ resources/vmnf_payloads.py
@@ -0,0 +1,29 @@
+import os
+import random
+import secrets
+from datetime import datetime
+from random import randint, choice
+import mimesis
+
+
+class VMNFPayloads:
+    def __init__(self, **settings):
+        '''VMNF Payloads'''
+
+        self.settings = settings
+        self.patterns = settings['patterns']
+    
+    def get_random_int(self):
+        return randint(0, 
+            choice(range(datetime.now().minute + datetime.now().second + len(self.patterns) * choice(bytes(range(256))))))
+    def get_random_unicode(self):
+        return choice(''.join(tuple(chr(i) for i in range(32, 0x110000) if chr(i).isprintable())))
+    def get_os_urandom(self):
+        return os.urandom(choice(range(18)))
+    def get_secure_random_string(self):
+        return secrets.token_urlsafe(choice(range(33)))
+    def get_random_float(self):
+        return random.random()
+    def get_random_credential(self):
+        gen = mimesis.Generic(choice([loc for loc in mimesis.locales.LIST_OF_LOCALES]))
+        return {'username':gen.person.username(),'password':gen.person.password()}
