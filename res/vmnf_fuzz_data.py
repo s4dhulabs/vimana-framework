3a2c619466d8d9730659399a7ef27f678a0612d0
s4dhu <s4dhul4bs@protonmail.ch>
2022-03-13 09:57:54 -0300

    :sparkles: general improvements

--- res/vmnf_fuzz_data.py
+++ res/vmnf_fuzz_data.py
@@ -13,13 +13,20 @@ class VMNFPayloads:
         '''VMNF Payloads'''
 
         self.settings = settings
-        self.patterns = settings['patterns']
+        self.patterns = settings.get('patterns', 10)
     
     def get_random_int(self):
         return randint(0, 
-            choice(range(datetime.now().minute + datetime.now().second + len(self.patterns) * choice(bytes(range(256))))))
+            choice(range(datetime.now().minute \
+            + datetime.now().second \
+            + len(self.patterns) \
+            * choice(bytes(range(256)))))
+        )
     def get_random_unicode(self):
-        return choice(''.join(tuple(chr(i) for i in range(32, 0x110000) if chr(i).isprintable())))
+        return choice(''.join(tuple(chr(i)\
+            for i in range(32, 0x110000) \
+                if chr(i).isprintable()))
+        )
     def get_os_urandom(self):
         return os.urandom(choice(range(18)))
     def get_secure_random_string(self):
@@ -27,14 +34,23 @@ class VMNFPayloads:
     def get_random_float(self):
         return random.random()
     def get_random_credential(self):
-        gen = mimesis.Generic(choice([loc for loc in mimesis.locales.LIST_OF_LOCALES]))
-        return {'username':gen.person.username(),'password':gen.person.password()}
+        gen = mimesis.Generic(
+            choice([loc for loc in mimesis.locales.LIST_OF_LOCALES])
+        )
+        return {
+            'username':gen.person.username(),
+            'password':gen.person.password()
+        }
     def get_ssti_payloads(self):
         with open(settings.ssti_p) as f:
-            return [p.strip() for p in f.readlines()[1:]]
+            return [p.strip('\n') for p in f.readlines()[1:]]
     def get_xss_payloads(self):
         with open(settings.xss_p) as f:
-            return [p.strip() for p in f.readlines()[1:]]
+            return [p.strip('\n') for p in f.readlines()[1:]]
     def get_sqli_payloads(self):
         with open(settings.sqli_p) as f:
-            return [p.strip() for p in f.readlines()[1:]]
+            return [p.strip('\n') for p in f.readlines()[1:]]
+    def get_pyvars(self):
+        with open(settings.common_pyvars) as f:
+            return [p.strip('\n') for p in f.readlines()[1:]]
+
